50fd41b66de9bdd4a171fc177e3d59382928cb9f
Paola0601 <pmaytaqu@unsa.edu.pe>
2024-10-31 13:16:43 -0500

    htmlheadl

--- cgi-bin/calculdora.pl
+++ cgi-bin/calculdora.pl
@@ -12,7 +12,15 @@ print $cgi->header;
 
 print <<HTML;
 
-
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width-device-width, initial-scale=1.0">
+  <title>Calculadora con perl</title>
+  <link rel="stylesheet" href="../css/styles.css">
+  
+</head>
 
 
 HTML
