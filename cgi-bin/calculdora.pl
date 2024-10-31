79c1477098115f6a4cced4106046497ab8abf42d
Paola0601 <pmaytaqu@unsa.edu.pe>
2024-10-31 11:32:39 -0500

    Creacion del objeto cgi

--- cgi-bin/calculdora.pl
+++ cgi-bin/calculdora.pl
@@ -2,4 +2,12 @@
 use strict;
 use warnings;
 use CGI qw(:standard);
-use CGI:: Carp qw(fatalsToBrowser);
+use CGI::Carp qw(fatalsToBrowser);
+
+#Creamos mi objeto CGI
+my $cgi=CGI->new;
+my $expression= $cgi->param('operacionMatematica');
+
+print $cgi->header;
+
+
