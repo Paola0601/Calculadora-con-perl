2084d7a42ddc1abe080c672496b1db09d7ac1c89
Paola0601 <pmaytaqu@unsa.edu.pe>
2024-10-31 20:43:27 -0500

    correcion5

--- cgi-bin/calculdora.pl
+++ cgi-bin/calculdora.pl
@@ -18,7 +18,7 @@ print <<HTML;
   <meta charset="UTF-8">
   <meta name="viewport" content="width-device-width, initial-scale=1.0">
   <title>Calculadora con perl</title>
-  <link rel="stylesheet" href="../css/styles.css">
+  <link rel="stylesheet" href="../css/estilo.css">
   
 </head>
 <body>
@@ -47,7 +47,7 @@ if (defined $operacionMatematica) {
 }
 
 print <<HTML;
-        <a href="/index.html">Regresar a la Página Principal</a>
+        <a href="/index.html">Calcular nueva expresion</a>
     </div>
 </body>
 </html>
